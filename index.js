f2881b7ee2d666f405db2d080a713fc49262c683
Gerald <kapgerado05@gmail.com>
2022-10-04 14:28:02 +0300

    Done with JavaScript function Continued lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+//The saturday activity is my fun
+function saturdayFun(myFun="roller-skate", This='bathe my dog') {
+   
+    return`This Saturday, I want to ${myFun}!`
+    return `This Saturday, I want to ${This}!`
+}
+saturdayFun(This);
+saturdayFun(myFun);
+
+
+function mondayWork(act_it = 'go to the office'){
+   
+    console.log(`This Monday, I will ${act_it}.`);
+    return `This Monday, I will ${act_it}.`
+}
+
+function wrapAdjective(sngl_prt1="*") {
+    return function(flair="special"){ 
+        return `You are ${sngl_prt1}${flair}${sngl_prt1}!` 
+     
+    }
+}
+
